a6b941b3cf3e48da14499521da3cd1c9531ab48a
Shashi Varshneya <shashikant9993@gmail.com>
2024-05-13 11:23:32 +0530

    did some changes and added some files

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+var movementStrength = 25;
+var height = movementStrength / window.screen.height;
+var width = movementStrength / window.screen.width;
+const hero = document.getElementById("section-hero");
+
+hero.addEventListener("mousemove", (e) => {
+    var pageX = e.clientX - window.screen.width / 2;
+    var pageY = e.clientY - window.screen.height / 2;
+    var newvalueX = width * pageX * -1 - 10;
+    var newvalueY = height * pageY * -1 - 0;
+    hero.style.backgroundPosition = newvalueX + "px     " + newvalueY + "px";
+
+    const div = document.createElement("div");
+    div.classList.add("hero-section-mouseClick");
+    div.style.left = e.clientX + "px";
+    div.style.top = e.clientY + "px";
+    const box = document.getElementById("section-hero");
+    box.appendChild(div);
+    setTimeout(() => {
+        box.removeChild(div);
+        div.remove();
+    }, 250);
+});
